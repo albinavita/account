22f70a39cce81a1155ec8569facb516cb0a49ddf
Albina <albina.melsitova@mail.ru>
2022-04-29 06:49:16 +0300

    Trying to add all classes

--- src/account/CheckingAccount.java
+++ src/account/CheckingAccount.java
@@ -1,16 +1,4 @@
 package account;
 
-public class CheckingAccount extends  Account{
-
-    public CheckingAccount(int balance){
-        super(balance);
-    }
-
-    public int getDebt(){
-        return (balance < 0) ? -balance : 0;
-    }
-    @Override
-    public void withdraw(int n){
-        balance -= n;
-    }
+public class CheckingAccount {
 }
